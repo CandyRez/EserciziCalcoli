25b30e6a17ce42dd2522ef903d0ee0cd89ee580a
CandyRez <121175251+CandyRez@users.noreply.github.com>
2022-12-23 16:10:16 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("insert variables");
+        float x = input.nextInt();
+        float y = input.nextInt();
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+    }
+}
